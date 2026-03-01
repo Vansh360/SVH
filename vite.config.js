223ca7cdc7a4de94e2ff4f91abb74b39090a624a
Vansh360 <badgayyavansh@gmail.com>
2026-03-01 20:41:48 +0530

    Added gallery images and updated Gallery.jsx component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/SVH/",
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/SVH/',   // 👈 important: match your deployment subfolder
   plugins: [react()],
-  base: "/SVH/",
-});
+})
